5676b408e89137a9ff562e740cab00c66f80f35c
dsan01 <dsanchezcol01@gmail.com>
2025-02-11 23:17:51 -0500

    feat(multi-lang): add i18n and new language support

---
+++ src/i18n/ui.ts
@@ -0,0 +1,42 @@
+export const languages = {
+  es: "Español",
+  en: "English",
+};
+
+export const defaultLang = "es";
+export const showDefaultLang = false;
+
+export const labels = {
+  es: {
+    "nav.projects": "Proyectos",
+    "nav.about": "Sobre Mi",
+    "nav.blog": "Blog",
+
+    "hero.title": "Desarrollador Web & Diseñador de Experiencias Digitales",
+    "hero.desc":
+      "Hola, soy David Sanchez. Transformo tus ideas en experiencias digitales únicas, uniendo creatividad y desarrollo para llevar tu proyecto al siguiente nivel.",
+    "hero.avalibe": "Disponible",
+    "hero.bento.ExperienceYears": "Años de experiencia",
+    "hero.bento.Tagline": "Hazlo a tu manera, con tus reglas",
+    "hero.bento.Description": "Diseños y desarrollo totalmente personalizados",
+    "service.develop" : "Desarrollo",
+    "service.desing" : "Diseño",
+    "service.managment" : "Gerencia",
+  },
+  en: {
+    "nav.projects": "Projects",
+    "nav.about": "About Me",
+
+    "hero.title": "Web Developer & Digital Experience Designer",
+    "hero.desc":
+      "Hi, I'm David Sanchez. I transform your ideas into unique digital experiences, bringing together creativity and development to take your project to the next level.",
+    "hero.avalibe": "Avalible",
+    "hero.bento.ExperienceYears": "Years of experience",
+    "hero.bento.Tagline": "Do it your way, with your rules",
+    "hero.bento.Description": "Fully customised designs and development",
+
+    "service.develop" : "Develop",
+    "service.desing" : "Desing",
+    "service.managment" : "Managment",
+  },
+} as const;
---
+++ src/i18n/utils.ts
@@ -0,0 +1,19 @@
+import { labels, defaultLang, showDefaultLang } from "./ui";
+
+export function getLangFromUrl(url: URL) {
+  const [, lang] = url.pathname.split("/");
+  if (lang in labels) return lang as keyof typeof labels;
+  return defaultLang;
+}
+
+export function useTranslations(lang: keyof typeof labels) {
+  return function t(key: keyof (typeof labels)[typeof defaultLang]) {
+    return labels[lang][key] || labels[defaultLang][key];
+  };
+}
+
+export function useTranslatedPath(lang: keyof typeof labels) {
+  return function translatePath(path: string, l: string = lang) {
+    return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`;
+  };
+}
