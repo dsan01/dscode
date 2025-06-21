bbe336e41e6069cb94c837889114018faba852a8
dsan01 <dsanchezcol01@gmail.com>
2025-06-20 19:47:44 -0500

    build: update and add new libs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,18 +7,26 @@ import icon from 'astro-icon';
 
 import react from "@astrojs/react";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
 
   integrations: [icon(), react()],
+
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "es",
     routing: {
       prefixDefaultLocale: false,
     }
-  }
+  },
+
+  adapter: node({
+    mode: "standalone"
+  })
 });
