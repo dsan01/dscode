0cf78e3d37664ed2cc6a02428c4d1e033aae56da
dsan01 <dsanchezcol01@gmail.com>
2025-07-27 12:44:40 -0500

    deat: update stack selector

--- src/components/ui/StackSelector.tsx
+++ src/components/ui/StackSelector.tsx
@@ -6,14 +6,21 @@ export const StackSelector: React.FC<StackSelectorProps> = ({
   text,
   isSelected = false,
   url,
+  onClick,
 }) => {
   const lang = getLangFromUrl(url);
   const t = useTranslations(lang);
   return (
-    <button className="group font-title flex items-center justify-center gap-3 border-l-2 border-transparent px-2.5 py-2 text-xl text-neutral-800 transition-colors hover:border-l-2 hover:border-l-neutral-500">
-      <span> {t(text)}</span>
-      <slot name="Icon" />
-      <TbArrowNarrowRight className="text-2xl transition-transform group-hover:-rotate-45" />
+    <button
+      className={`group font-title flex items-center justify-between gap-3 border-l-2 border-transparent px-2.5 py-2 text-xl text-neutral-800 transition-colors ${isSelected ? "border-l-primary-700 border-l-2" : "cursor-pointer hover:border-l-2 hover:border-l-neutral-500"}`}
+      onClick={() => onClick()}
+    >
+      <span> {t(text) ?? text} </span>
+      <div>
+        <TbArrowNarrowRight
+          className={`text-2xl transition-transform ${isSelected ? "-rotate-45" : "group-hover:-rotate-45"}`}
+        />
+      </div>
     </button>
   );
 };
